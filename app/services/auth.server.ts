51f73f4e9858b4d0be599ba089f1517d797e0344
yab <hello@factions.ai>
2024-06-10 09:15:54 -0400

    x

---
+++ app/services/auth.server.ts
@@ -0,0 +1,13 @@
+/// Login Function - takes email and password
+///
+
+
+// Don't be afraid of cookes, they're basically just...
+// const Cookies = {
+//     "key1": "value1",
+//     "key2": "value2",
+// }
+
+
+// Authenticator
+// 
