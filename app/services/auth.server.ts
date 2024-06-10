78bfe2eddb105e375ce8653531ffa1e0794249c6
yab <hello@factions.ai>
2024-06-10 09:21:48 -0400

    copy/paste from https://github.com/sergiodxa/remix-auth?tab=readme-ov-file

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -1,3 +1,47 @@
+// app/services/session.server.ts
+import { createCookieSessionStorage } from "@remix-run/node";
+import { Authenticator } from "remix-auth";
+import { FormStrategy } from "remix-auth-form";
+
+export const authenticator = new Authenticator<User>(sessionStorage);
+
+// Tell the Authenticator to use the form strategy
+authenticator.use(
+  new FormStrategy(async ({ form }) => {
+    let email = form.get("email");
+    let password = form.get("password");
+    let user = await login(email, password);
+    // the type of this user must match the type you pass to the Authenticator
+    // the strategy will automatically inherit the type if you instantiate
+    // directly inside the `use` method
+    return user;
+  }),
+  // each strategy has a name and can be changed to use another one
+  // same strategy multiple times, especially useful for the OAuth2 strategy.
+  "user-pass"
+);
+
+interface UserAuthInfo {
+    email: string,
+    password: string,
+}
+
+// export the whole sessionStorage object
+export const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "_session", // use any name you want here
+    sameSite: "lax", // this helps with CSRF
+    path: "/", // remember to add this so the cookie will work in all routes
+    httpOnly: true, // for security reasons, make this cookie http only
+    secrets: ["s3cr3t"], // replace this with an actual secret
+    secure: process.env.NODE_ENV === "production", // enable this in prod only
+  },
+});
+
+// you can also export the methods individually for your own usage
+export let { getSession, commitSession, destroySession } = sessionStorage;
+
+
 /// Login Function - takes email and password
 ///
 
@@ -11,3 +55,6 @@
 
 // Authenticator
 // 
+
+
+
